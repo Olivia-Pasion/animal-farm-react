606baee3c6a85f32bb673dde6bd505ecf93d9bc9
olivia pasion <oap9820@gmail.com>
2022-09-12 14:20:44 -0700

    animals display

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './Main.css';
 import backgroundImage from '../../background.png';
+import { animals } from '../../data.js';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${backgroundImage})` }}></main>;
+  return <main style={{ backgroundImage: `url(${backgroundImage})` }}>
+    {animals.map((item) => (
+      <Animal key={item.name} {...item} />
+    ))}
+  </main>;
 }
